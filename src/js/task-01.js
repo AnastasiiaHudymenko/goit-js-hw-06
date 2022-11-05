6ab6c0291d6b582d8008f917c6c2fde673a661c5
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2022-11-05 14:56:49 +0200

    add js.

---
+++ src/js/task-01.js
@@ -0,0 +1,31 @@
+// знаходжу загальну к-сть категорій
+const listEl = document.querySelector("#categories");
+const allItemEl = listEl.querySelectorAll(".item");
+console.log(`Number of categories: ${allItemEl.length}`);
+
+// знаходжу першу категорію (Animal)
+const firstItemEl = listEl.firstElementChild;
+const firstTitleEl = firstItemEl.querySelector("h2").textContent;
+console.log(`Category: ${firstTitleEl}`);
+
+// знаходжу загальну к-сть елементів Animal
+const allItemChildinFirstItem = firstItemEl.querySelectorAll("li").length;
+console.log(`Elements: ${allItemChildinFirstItem}`);
+
+// знаходжу першу категорію (Products)
+const secondItemEl = firstItemEl.nextElementSibling;
+const secondTitleEl = secondItemEl.querySelector("h2").textContent;
+console.log(`Category: ${secondTitleEl}`);
+
+// знаходжу загальну к-сть елементів Products
+const allItemChildinSecondItem = secondItemEl.querySelectorAll("li").length;
+console.log(`Elements: ${allItemChildinSecondItem}`);
+
+// знаходжу першу категорію (Technologies)
+const lastItemEl = listEl.lastElementChild;
+const lastTitleEl = lastItemEl.querySelector("h2").textContent;
+console.log(`Category: ${lastTitleEl}`);
+
+// знаходжу загальну к-сть елементів Technologies
+const allItemChildinLastItem = lastItemEl.querySelectorAll("li").length;
+console.log(`Elements: ${allItemChildinLastItem}`);
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,47 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// Знаходимо наш ul
+const listEl = document.querySelector("#ingredients");
+// console.log(listEl);
+
+// Створюємо li і додаємо класс
+const itemElOne = document.createElement("li");
+itemElOne.textContent = "Potatoes";
+itemElOne.classList.add("item");
+
+const itemElTwo = document.createElement("li");
+itemElTwo.textContent = "Mushrooms";
+itemElTwo.classList.add("item");
+
+const itemElThree = document.createElement("li");
+itemElThree.textContent = "Garlic";
+itemElThree.classList.add("item");
+
+const itemElFour = document.createElement("li");
+itemElFour.textContent = "Tomatos";
+itemElFour.classList.add("item");
+
+const itemElFive = document.createElement("li");
+itemElFive.textContent = "Herbs";
+itemElFive.classList.add("item");
+
+const itemElSix = document.createElement("li");
+itemElSix.textContent = "Condiments";
+itemElSix.classList.add("item");
+
+//додавання елементів
+listEl.append(
+  itemElOne,
+  itemElTwo,
+  itemElThree,
+  itemElFour,
+  itemElFive,
+  itemElSix
+);
